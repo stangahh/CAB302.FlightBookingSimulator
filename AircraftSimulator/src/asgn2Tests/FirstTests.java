61cdeb7fc9dee3b6e61226d4ddbed8af4f851ea6
Nyx- <mnhunter@outlook.com>
2016-05-28 23:21:32 +1000

    Fixed firstTests creation of Passengers in each test and made small changes to the A380Tests

--- AircraftSimulator/src/asgn2Tests/FirstTests.java
+++ AircraftSimulator/src/asgn2Tests/FirstTests.java
@@ -18,7 +18,7 @@ import asgn2Passengers.PassengerException;
  */
 public class FirstTests extends First {
 
-	static Passenger person;
+	Passenger person;
 	
 	/**
 	 * @throws java.lang.Exception
@@ -27,54 +27,46 @@ public class FirstTests extends First {
 	public void setUpBeforeClass() throws PassengerException {
 		//CAN'T INSTANTIATE AN ABSTRACT CLASS
 		//This before doesn't work with the test cases for some reason...
-		Passenger person = new First(2,10);
+		this.person = new First(2,10);
 	}
 
 	@Test
 	public void testBookingTime() throws PassengerException {
-		Passenger person = new First(2,10);
 		assertEquals(2, person.getBookingTime());
 	}
 	
 	@Test
 	public void testDepartureTime() throws PassengerException {
-		Passenger person = new First(2,10);
 		assertEquals(10, person.getDepartureTime());
 	}
 	
 	@Test
 	public void testNewState() throws PassengerException {
-		Passenger person = new First(2,10);
 		assertTrue(person.isNew());
 	}
 	
 	@Test
 	public void testConfirmSeatValid() throws PassengerException {
-		Passenger person = new First(2,10);
 		person.confirmSeat(5, person.getDepartureTime());
 	}
 	
 	@Test (expected = PassengerException.class)
 	public void testConfirmSeatLessThanZeroConfirmationTime() throws PassengerException {
-		Passenger person = new First(2,10);
 		person.confirmSeat(-1, person.getDepartureTime());
 	}
 	
 	@Test (expected = PassengerException.class)
 	public void testConfirmSeatConfirmationTimeAfterDepartureTime() throws PassengerException {
-		Passenger person = new First(2,10);
 		person.confirmSeat(11, person.getDepartureTime());
 	}
 	
 	@Test (expected = PassengerException.class)
 	public void testConfirmSeatConfirmationTimeBeforeBookingTime() throws PassengerException {
-		Passenger person = new First(2,10);
 		person.confirmSeat(1, person.getDepartureTime());
 	}
 	
 	@Test
 	public void testCancelSeat() throws PassengerException {
-		Passenger person = new First(2,10);
 		person.confirmSeat(3, person.getDepartureTime());
 		person.cancelSeat(5);
 		
